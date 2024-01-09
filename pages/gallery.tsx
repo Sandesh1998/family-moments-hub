59176ad5ee6fd69395025c1e2aff221155438123
Sandesh1998 <sandeshpoudel144@gmail.com>
2024-01-09 20:02:12 +0545

    feat : image gallery

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -14,23 +14,27 @@ type Image = {
 };
 
 function Gallery() {
- const [selectedFile, setSelectedFile] = useState<File | null>(null);
+ const [selectedFiles, setSelectedFiles] = useState<FileList | null>(null);
  const [images, setImages] = useState<Image[]>([]);
  const inputRef = React.useRef<HTMLInputElement | null>(null);
 
  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
- setSelectedFile(e.target.files ? e.target.files[0] : null);
+ if (e.target.files) {
+ setSelectedFiles(e.target.files);
+ }
  };
 
  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
  e.preventDefault();
 
- if (!selectedFile) {
+ if (!selectedFiles || selectedFiles.length === 0) {
  return;
  }
 
  const formData = new FormData();
- formData.append('photos', selectedFile);
+ Array.from(selectedFiles).forEach((file) => {
+ formData.append('photos', file);
+ });
 
  const response = await fetch('http://localhost:5000/familyMoment/api/upload', {
  method: 'POST',
@@ -38,11 +42,11 @@ function Gallery() {
  });
 
  if (response.ok) {
- console.log('File uploaded successfully');
+ console.log('Files uploaded successfully');
  fetchImages(); // Refresh the list of images after uploading a new one
  inputRef.current!.value = ''; // Clear the input field
  } else {
- console.error('Failed to upload file');
+ console.error('Failed to upload files');
  }
  };
 
@@ -63,24 +67,21 @@ function Gallery() {
  return (
  <>
  <div className="flex flex-col justify-center items-center gap-1.5">
- <Label htmlFor="picture">Picture of the day</Label>
+ <Label htmlFor="picture">Pictures of the day</Label>
  <Input
  ref={inputRef}
  id="picture"
  type="file"
  onChange={handleFileChange}
+ multiple
  className="w-[600px] mx-auto file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100 file:border file:border-solid file:border-blue-700 file:rounded-md border-blue-600"
  />
  <button type="submit" onClick={handleSubmit} className="bg-green-500 text-white px-4 py-2 rounded">Submit</button>
  </div>
  <div className="grid grid-cols-5 gap-4 gap-y-0.5 px-2">
- {images.map((image) => (
- <div key={image._id}>
- {image.images.map((img) => (
- <img src={ `${img.path}`} key={img._id} alt={img.filename} className="object-contain w-[500px] h-[210px]" />
- ))}
- </div>
- ))}
+ {images.flatMap((image) => image.images.map((img) => (
+  <img src={`${img.path}`} key={img._id} alt={img.filename} className="object-contain w-full h-[220px]" />
+ )))}
  </div>
  </>
  );
